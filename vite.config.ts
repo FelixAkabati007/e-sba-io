10598cf953813a9537fc6a4eaaf4d3d6718871e4
FelixAkabati007 <felixakabati007@example.com>
2026-01-13 22:36:20 -1200

    VERCEL PRODUCTION UPDATE 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: ["es2018", "chrome64", "edge79", "firefox67", "safari13"],
+    cssTarget: "safari13",
+  },
   server: {
     host: true,
     port: 5173,
